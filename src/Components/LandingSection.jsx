74849b66f4c5900e635c480c613ceeecf4081a9f
Akash M Nandan <akash003nandan@gmail.com>
2025-01-25 22:05:38 +0530

    feat: Design for HeroSection (Desktop View)

--- src/Components/LandingSection.jsx
+++ src/Components/LandingSection.jsx
@@ -3,28 +3,30 @@ import search from '../assets/Images/Search_icon.png'
 
 const LandingSection = () => {
   return (
-    <div className="h-screen w-[100%]">
-        <img src={bg} alt="Hero_Bg_Image" className='-mt-5 w-full h-[1068px]'/>
-        <div className='-mt-[580px] flex display justify-center items-center flex-col gap-[50px]'>
-            <p className='text-[65px] font-[700] text-white'>Where Paradise Feels Like Home</p>
-            <div className='w-[600px] h-[74px] bg-white rounded-[50px] flex flex-row justify-center items-center gap-12'>
-                <div className='flex flex-col'>
-                    <p className='text-xs'>Who</p>
-                    <p className='text-sm opacity-60 font-[400]'>Add guests</p>
-                </div>
-                <p className='h-[35px] w-[1px] bg-black opacity-20'></p>
-                <div className='flex flex-col'>
-                    <p className='text-xs'>Who</p>
-                    <p className='text-sm opacity-60 font-[400]'>Add guests</p>
-                </div>
-                <p className='h-[35px] w-[1px] bg-black opacity-20'></p>
-                <div className='flex flex-col'>
-                    <p className='text-xs'>Who</p>
-                    <p className='text-sm opacity-60 font-[400]'>Add guests</p>
-                </div>
-                <button className='bg-green-800 h-[44px] w-[44px] flex items-center justify-center rounded-full'><img src={search} alt="seacrh_icon" height={16} width={16} /></button>
-            </div>
+    <div className="h-full w-full relative">
+      <img src={bg} alt="Hero_Bg_Image" className='top-0 left-0 w-full h-full object-cover' />
+      <div className='lg:-mt-[630px] z-10 w-full h-full flex justify-center items-center flex-col gap-8 px-4 text-center'>
+        <p className='text-3xl sm:text-4xl md:text-5xl lg:text-[65px] font-bold text-white leading-tight max-w-[90%]'>Where Paradise Feels Like Home</p>
+        <div className='w-full max-w-[90%] sm:max-w-[600px] h-auto sm:h-[74px] bg-white rounded-full flex flex-col sm:flex-row justify-between items-center px-6 py-4 sm:py-0 gap-4 sm:gap-6 shadow-md'>
+          <div className='flex flex-col text-center w-full sm:w-auto'>
+            <p className='text-xs md:text-sm'>Who</p>
+            <p className='text-sm md:text-base opacity-60 font-normal'>Add guests</p>
+          </div>
+          <div className='h-[1px] sm:h-[35px] w-full sm:w-[1px] bg-black opacity-20'></div>
+          <div className='flex flex-col text-center w-full sm:w-auto'>
+            <p className='text-xs md:text-sm'>When</p>
+            <p className='text-sm md:text-base opacity-60 font-normal'>Add dates</p>
+          </div>
+          <div className='h-[1px] sm:h-[35px] w-full sm:w-[1px] bg-black opacity-20'></div>
+          <div className='flex flex-col text-center w-full sm:w-auto'>
+            <p className='text-xs md:text-sm'>Where</p>
+            <p className='text-sm md:text-base opacity-60 font-normal'>Add location</p>
+          </div>
+          <button className='bg-green-800 h-10 w-10 sm:h-11 sm:w-11 flex items-center justify-center rounded-full'>
+            <img src={search} alt="search_icon" className='w-4 h-4' />
+          </button>
         </div>
+      </div>
     </div>
   )
 }
