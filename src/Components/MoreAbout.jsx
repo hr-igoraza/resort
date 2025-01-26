bcaa805a6bbd06704b769a321fc434c5792ac41c
Akash M Nandan <akash003nandan@gmail.com>
2025-01-26 21:21:24 +0530

    feat: MoreAbout Section (responsive view)

--- src/Components/MoreAbout.jsx
+++ src/Components/MoreAbout.jsx
@@ -4,10 +4,10 @@ import bg from '../assets/Images/moreAbout.jpg'
 const MoreAbout = () => {
   return (
     <div className='w-full h-[700px]'>
-        <img src={bg} alt="" className='w-full h-[700px] object-cover' />
-        <div className='flex-col flex gap-[100px] max-sm:-mt-[700px] -mt-[500px] md:px-[100px]'>
-            <p className='text-white text-[90px] font-bold leading-[110px]'>Escape to Paradise</p>
-            <p className='text-white text-[20px] font-medium w-[469px] h-[218px] max-sm:px-4'>Discover the perfect getaway where tranquility meets luxury. Immerse yourself in the serene beauty of nature, unwind by the pristine waters, and experience unparalleled hospitality. Whether you're seeking adventure or relaxation, our resort offers everything you need to create unforgettable memories.</p>
+        <img src={bg} alt="" className='w-full h-[700px] max-sm:h-[650px] object-cover' />
+        <div className='flex-col flex gap-[100px] max-sm:-mt-[700px] -mt-[500px] md:px-[100px] max-md:py-[100px] max-md:-mt-[700px] max-sm:py-[100px] max-sm:px-[10px]'>
+            <p className='text-white text-[90px] max-sm:text-7xl font-bold leading-[110px] max-sm:leading-[70px]'>Escape to Paradise</p>
+            <p className='text-white text-[20px] font-medium w-[469px] h-[218px] max-sm:h-[200px] max-sm:w-full max-sm:px-4 md:px-[20px]'>Discover the perfect getaway where tranquility meets luxury. Immerse yourself in the serene beauty of nature, unwind by the pristine waters, and experience unparalleled hospitality. Whether you're seeking adventure or relaxation, our resort offers everything you need to create unforgettable memories.</p>
         </div>
     </div>
   )
