da60a69a2e5c8f03b9051375305cfe07b4683e48
Akash M Nandan <akash003nandan@gmail.com>
2025-01-27 01:16:46 +0530

    feat: cuisine  designed (mobile view)

--- src/Components/StayAndCuisine.jsx
+++ src/Components/StayAndCuisine.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import room1 from '../assets/Images/room1.png'
 import room2 from '../assets/Images/room2.png'
 import room3 from '../assets/Images/room3.png'
+import  flavours from '../assets/Images/flavours.png'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 const StayAndCuisine = () => {
   return (
-    <div className='py-[100px] max-sm:py-0 lg:px-[100px]'>
+    <div className='py-[100px] max-sm:py-0 lg:px-[100px] gap-[100px]'>
         {/* stay */}
-        <div className='flex flex-col max-sm:flex-col-reverse lg:flex-row items-center justify-center gap-10'>
+        <div className='flex flex-col max-sm:flex-col-reverse lg:flex-row items-center justify-center gap-10 pb-[100px]'>
         {/* images for large screens */}
         <div className='hidden lg:flex flex-row gap-8'>
           <img src={room1} alt="Room 1" className='w-[300px] h-auto shadow-lg' />
@@ -41,9 +42,7 @@ const StayAndCuisine = () => {
               Comfort & Luxury Book Your Perfect Stay
             </p>
             <p className='text-[#4C4850] text-[16px] font-normal tracking-wider'>
-            Step into a world where modern elegance meets unmatched comfort. From plush interiors to breathtaking views, every detail of our resort is designed to provide a luxurious retreat. Whether you’re unwinding in our premium suites or lounging by the serene poolside, every moment here feels like a dream come true.
-
-            </p>
+            Step into a world where modern elegance meets unmatched comfort. From plush interiors to breathtaking views, every detail of our resort is designed to provide a luxurious retreat. Whether you’re unwinding in our premium suites or lounging by the serene poolside, every moment here feels like a dream come true.</p>
           </div>
           {/* button */}
           <button className='w-[200px] h-[60px] bg-[#025D28] lg:-mr-[200px] lg:mx-0'>
@@ -53,21 +52,25 @@ const StayAndCuisine = () => {
       </div>
 
         {/* cuisine */}
-        <div>
+        <div className='flex flex-col lg:flex-row items-start justify-center gap-10 pb-[100px]'>
             {/* text and button */}
-            <div>
+            <div className='flex flex-col gap-10 items-start justify-start px-[30px]'>
                 {/* text */}
-                <div>
-
+                <div className='w-full lg:w-[550px] flex flex-col gap-10'>
+                    <p className='text-[40px] md:text-[50px] lg:text-[65px] md:leading-[70px] font-bold tracking-wide'>
+                    Flavors of Kerala Authentic Cuisine at Our Resort
+                    </p>
+                    <p className='lg:w-[455px] text-[#4C4850] text-[16px] font-normal tracking-wider'>
+                    Delight in the soul of Kerala with our authentic culinary offerings. Our expert chefs bring you an array of traditional dishes, crafted with locally sourced spices and ingredients. From rich curries to freshly steamed delicacies, each bite is a celebration of Kerala’s vibrant flavors. Pair your meal with our signature beverages for a dining experience that’s as unique as the land itself.</p>
                 </div>
                 {/* button */}
-                <button>
-
+                <button className='w-[200px] h-[60px] bg-[#025D28]'>
+                    <p className='text-[20px] font-normal leading-[40px] text-white'>See more</p>
                 </button>
             </div>
 
             {/* image */}
-            <img src="" alt="" />
+            <img src={flavours} alt="" />
         </div>
     </div>
   )
